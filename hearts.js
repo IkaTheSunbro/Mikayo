8a48f42015689613a7b1e70ad2ca005ed5cbd01e
Ika The Sunbro <82341724+IkaTheSunbro@users.noreply.github.com>
2024-09-30 14:59:28 +0000

    boing

---
+++ hearts.js
@@ -0,0 +1,19 @@
+function hearts(){
+    let e = document.createElement('div');
+    e.setAttribute('class', 'heart');
+    document.body.appendChild(e);
+
+    e.style.left = Math.random() * + innerWidth + 'px';
+
+    let size = Math.random() * 12;
+
+    e.style.fontSize = 12 + size+'px';
+
+    setTimeout(function(){
+        document.body.removeChild(e);
+    }, 5000)
+}
+
+setInterval(function() {
+    hearts()
+}, 300)
