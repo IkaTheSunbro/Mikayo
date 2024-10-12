464366d50b839fe830488adb82c4f196f09e5d92
Ika The Sunbro <82341724+IkaTheSunbro@users.noreply.github.com>
2024-10-12 16:53:16 +0000

    alien kitty

--- stuff/kitty.js
+++ stuff/kitty.js
@@ -1,16 +1,22 @@
 function spawnKitty(){
 
-    const audio = new Audio('media/meow.mp3'); // Path to your meow sound
-    audio.play();
+    const meowAudio = new Audio('media/meow.mp3'); // Path to your meow sound
+    const zipAudio = new Audio('media/zip.mp3'); // Path to your meow sound
 
     let e = document.createElement('img');
     e.setAttribute('class', 'kitty');
 
-    if(Math.random() > 0.5){
-        e.src = "media/IkaKitty.gif"
+    if(Math.random() > 0.99){
+        e.src = "media/AlienKitty.gif"
+        zipAudio.play();
     }
-    else{
-        e.src = "media/MiyoKitty.gif"
+    else if(Math.random() > 0.5) {
+        e.src = "media/IkaKittyNew.gif"
+        meowAudio.play();
+    }
+    else {
+        e.src = "media/MiyoKittyNew.gif"
+        meowAudio.play();
     }
 
     document.body.appendChild(e);
